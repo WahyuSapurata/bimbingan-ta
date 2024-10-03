68ef81a8504acbd19798204153c307a4d99b93e7
WahyuSapurata <wm337708@gmail.com>
2024-10-03 20:41:45 +0800

    update channels log

--- routes/channels.php
+++ routes/channels.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,19 +16,34 @@ use Illuminate\Support\Facades\Broadcast;
 
 // Untuk channel chat
 Broadcast::channel('presence-chat.{chatId}', function ($user, $chatId) {
+    // Log data awal
+    Log::info('Checking presence-chat channel:', [
+        'user_uuid' => $user->uuid,
+        'chatId' => $chatId,
+    ]);
+
     // Pisahkan chatId menjadi dua UUID
     $uuids = explode('_', $chatId);
+    Log::info('Parsed UUIDs:', ['uuids' => $uuids]);
+
     if (count($uuids) !== 2) {
+        Log::warning('Invalid chatId format:', ['chatId' => $chatId]);
         return false;
     }
 
     // Pastikan pengguna adalah salah satu dari kedua UUID
     if (!in_array($user->uuid, $uuids)) {
+        Log::warning('User not authorized for chatId:', [
+            'user_uuid' => $user->uuid,
+            'chatId' => $chatId,
+        ]);
         return false;
     }
 
     // Mengembalikan data pengguna untuk Presence Channel
-    return ['uuid' => $user->uuid, 'name' => $user->name];
+    $presenceData = ['uuid' => $user->uuid, 'name' => $user->name];
+    Log::info('User authorized for channel:', $presenceData);
+    return $presenceData;
 });
 
 Broadcast::channel('notifications.{student_uuid}', function ($user, $student_uuid) {
